a550f658e57bd3786eb95f0febae1741784b7dd8
Federico Martinez-Seidel <54093698+MSeidelFed@users.noreply.github.com>
2020-10-12 16:43:54 +1100

    Take steady state pools from the raw abundances

--- R_Functions/IncorporationProxys.R
+++ R_Functions/IncorporationProxys.R
@@ -9,7 +9,7 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
   
   split_ <- function(x) {strsplit(x = x, split = "_")[1]}
   
-    list2df <- function(x) 
+  list2df <- function(x) 
   { 
     MAX.LEN <- max(sapply(x, length), na.rm = TRUE) 
     DF <- data.frame(lapply(x, function(x) c(x, rep(NA, MAX.LEN - length(x))))) 
@@ -24,15 +24,18 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
     Enrichment_files <- list.files(path = Parent_dir, pattern = "Corrected.csv",
                                    full.names = T, recursive = T)
     
+    RawData <- list.files(path = Parent_dir, pattern = "RawData.csv",
+                          full.names = T, recursive = T)
+    
     MolecularSpecies <- unique(apply(list2df(sapply(X = rownames(read.csv(Enrichment_files[1],
-                                                                   header = T, row.names = 1)),
-                                                      FUN = split_))[1,],
-                                       MARGIN = 2,
-                                       FUN = as.character))
+                                                                          header = T, row.names = 1)),
+                                                    FUN = split_))[1,],
+                                     MARGIN = 2,
+                                     FUN = as.character))
     
     SteadyStatePoolsM1M0 <- matrix(NA, nrow = length(Enrichment_files),
                                    ncol = length(MolecularSpecies))
-      
+    
     SteadyStatePoolsM1Mn <- matrix(NA, nrow = length(Enrichment_files),
                                    ncol = length(MolecularSpecies))
     
@@ -42,9 +45,13 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
       
       Enrichment_file_runner <- Enrichment_files[i]
       
+      Raw_file_runner <- RawData[i]
+      
       ### runner indexes
       
       File_i <- read.csv(file = Enrichment_file_runner, header = T, row.names = 1)
+      
+      Raw_File_i <- read.csv(file = Raw_file_runner, header = T, row.names = 1)
       
       MolecularSpecies_i <- unique(apply(list2df(sapply(X = rownames(File_i) , FUN = split_))[1,],
                                          MARGIN = 2,FUN = as.character))
@@ -53,7 +60,7 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
                          split = "\\.")[[1]][1]
       
       treatment_i <- strsplit(strsplit(strsplit(Enrichment_file_runner, split = "/")[[1]][5],
-                              split = "\\.")[[1]][1], split = "_")[[1]][2]
+                                       split = "\\.")[[1]][1], split = "_")[[1]][2]
       
       rep_i <- strsplit(strsplit(strsplit(Enrichment_file_runner, split = "/")[[1]][5],
                                  split = "\\.")[[1]][1], split = "_")[[1]][3]
@@ -79,7 +86,7 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
       
       write.csv(M1fraction, paste0(out_path_i, name_i, "M1fraction.csv"))
       
-      ## need to grab isotopologues per lipid and then build steady state pools 
+      ## need to grab isotopologues per lipid from the raw data and then build steady state pools 
       ## either from M0+M1 or from M0+...+Mn
       
       means_isotopologues <- c()
@@ -90,7 +97,7 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
         
         #print(MolecularSpecies_i[j])
         
-        lipid_i <- File_i[grep(pattern = MolecularSpecies_i[j], x = rownames(File_i)),]
+        lipid_i <- Raw_File_i[grep(pattern = MolecularSpecies_i[j], x = rownames(File_i)),]
         
         means_isotopologues[j] <- mean(colSums(lipid_i))
         
@@ -113,25 +120,25 @@ IncorporationProxys <- function(Parent_dir, SteadyStatePools_dir = NULL) {
     
     if (is.null(SteadyStatePools_dir)) {
       
-      write.csv(SteadyStatePoolsM1Mn, file = "SteadyStatePoolsM1Mn.csv")
+      write.csv(SteadyStatePoolsM1Mn, file = "SteadyStatePoolsM0Mn.csv")
       
-      write.csv(SteadyStatePoolsM1M0, file = "SteadyStatePoolsM1M0.csv")
+      write.csv(SteadyStatePoolsM1M0, file = "SteadyStatePoolsM0M1.csv")
       
     } else {
       
       write.csv(SteadyStatePoolsM1Mn, file = paste0(SteadyStatePools_dir,
-                                                    "/", "SteadyStatePoolsM1Mn.csv"))
+                                                    "/", "SteadyStatePoolsM0Mn.csv"))
       
       write.csv(SteadyStatePoolsM1M0, file = paste0(SteadyStatePools_dir,
-                                                    "/", "SteadyStatePoolsM1M0.csv"))
+                                                    "/", "SteadyStatePoolsM0M1.csv"))
     }
     
-      
+    
   } else {
-      
+    
     print("IsoCorrectoR directories must be three levels below, e.g.: ParentDir/xx/20...")
-      
+    
   }
-    
-    
+  
+  
 }
