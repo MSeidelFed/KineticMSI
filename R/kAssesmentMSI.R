37ebb81be58eda0e0a5c66c46594b0a2b6a26581
Federico Martinez-Seidel <54093698+MSeidelFed@users.noreply.github.com>
2021-10-11 13:49:13 +0200

    additions
    + Option to deal with zeros in datasets
    
    + change in object named Class comparison input to RowMeansDataset

--- R/kAssesmentMSI.R
+++ R/kAssesmentMSI.R
@@ -1,14 +1,15 @@
-#' A function to perform an initial step of replicated data quality assessment on KineticMSI datsets
+#' A function to perform an initial step of replicated data quality assessment on KineticMSI datasets
 #'
 #' This function allows KineticMSI users to assess the quality in terms of reproducibility and mean distribution from measured molecular features across several independent treatments. The function first subsets all datasets to a common vector of molecular features and a common number of pixels. If molecular features are not the same across datatsets, the function produces new sets with the "Shared Features", which are named with this extension in the original IsoCorrectoR folders. Pixels are previously sorted per sample according to their magnitude, e.g., in the case of enrichment percentage from high to low enrichment. Afterwards a plot reflecting the ratio of means from subset and entire sets is produced in order to evaluate whether the subsetting procedure is skewing the data or whether the change trends will be fully preserved. An alternative to sorting by magnitude is sorting by acquisition time, which allows the users later on to evaluate differences that might be constrained in space or dependent on the instrument performance during a single sample. The function returns distribution plots in a PDF file across treatments that allow interpreting shifts in data that would otherwise remain unnoticed. Finally the function also returns either a list with the minimum datasets for all matrices or the compressed matrices used for mean distribution assessment and later for mean class comparison across samples.
 #' @param path parent directory were all enrichment files are contained, digs within recursive folders.
 #' @param PatternEnrichment defaults to "MeanEnrichment". Defines a character vector used to grab input csv enrichment files that can be later subset.
 #' @param SubSetRepsIntensities defaults to FALSE. Allows to subset the MSI file list found in path.
 #' @param CompareSampledSet defaults to TRUE. Compares the subsets and entire sets by plotting the ratios between mean and standard deviations across molecular features to allow evaluation of biased sampling. Optimally the ratios should oscillate around 1 with minimum variance.
-#' @param returnObject can be either "ClassComparisonInput" or "minDataset" and returns either a list with input matrices per molecular feature for the following mean class comparison function or the minimum data set subset to the minimum amount of pixels.
+#' @param returnObject can be either "RowMeansDataset" or "minDataset" and returns either a list with input matrices per molecular feature for the following mean class comparison (only the rowmeans per feature) or the minimum data subset to the minimum amount of pixels per entity.
 #' @param factorVector character vector that needs to define the treatments using the same nomenclature and naming scheme as for the input files (follow the exemplary KineticMSI data for details).
 #' @param fun2clust allows to decide whether pixels across files are sorted before subseting using their magnitude, i.e., "Enrichment" or their acquisition time, i.e., "AcqTime". 
-#' @param logiTransformation Allows users to perform logit transformation before the class distribution assessment.
+#' @param ZeroAction allows to define what happens with zeros within the entity data matrices. If null, the zeros are fully preserved, if "remove" the rows that only contain zeros are deleted and if "replace" all zeros are replaced by normally distributed randomly generated numbers in the scale of 10^-12 to 10^-13.
+#' @param logiTransformation allows users to perform logit transformation before the class distribution assessment.
 #' @param ScalingFactorXaxisDensityPlot defaults to NULL = 1 and defines the boundary of the x-axis in the ggplots that assess the distribution of the data.
 #' @keywords MSI Replicates Assessment Tracer Dynamics
 #' @export
@@ -21,9 +22,10 @@ kAssesmentMSI <- function(path,
                           PatternEnrichment = "MeanEnrichment",
                           SubSetRepsIntensities = FALSE,
                           CompareSampledSet = TRUE,
-                          returnObject = c(NULL, "ClassComparisonInput", "minDataset"),
+                          returnObject = c(NULL, "RowMeansDataset", "minDataset"),
                           factorVector,
                           fun2clust = c("Enrichment", "AcqTime"),
+                          ZeroAction = c(NULL, "remove", replace),
                           logiTransformation = F,
                           ScalingFactorXaxisDensityPlot = NULL) {
   
@@ -162,7 +164,7 @@ kAssesmentMSI <- function(path,
     
     cat("...\n")
     cat(paste0("Processing: ", feature_Nr[i]," - ", i, "\n"))
-    cat("...\n")
+    
     
     if (fun2clust == "Enrichment") {
       
@@ -180,13 +182,40 @@ kAssesmentMSI <- function(path,
       
     } else {
       
-      cat("Error: select an appropriate sorting function")
+      stop("Error: select an appropriate sorting function")
       
     }
     
     colnames(runner[[1]]) <- paste0(rep("X", dim(runner[[1]])[2]), c(1:dim(runner[[1]])[2]))
     
-    runner_WO_zeros <- as.matrix(runner[[1]][,as.numeric(colSds(runner[[1]])) != 0])
+    
+    if(is.null(ZeroAction)) {
+      
+      runner_WO_zeros <- as.matrix(runner[[1]])
+      
+    } else if(ZeroAction == "remove") {
+      
+      runner_WO_zeros <- as.matrix(runner[[1]][,as.numeric(colSds(runner[[1]])) != 0])
+      
+    } else if(ZeroAction == "replace") {
+      
+      if(length(which(colSds(runner[[1]]) == 0)) > 0) {
+        
+        runner[[1]][runner[[1]] == 0] <- rnorm(length(which(runner[[1]] == 0)),
+                                               mean = 0.0000000000001,
+                                               sd = 0.00000000001)
+      }
+      
+      runner_WO_zeros <- as.matrix(runner[[1]])
+      
+    }
+    
+    cat(paste0("Mean: ",
+               round(mean(runner[[1]]), digits = 3),
+               "; St.Dev: ",
+               round(sd(runner[[1]]), digits = 3), "\n"))
+    cat("...\n")
+    
     
     #### transforming to logit if set to TRUE
     
@@ -261,7 +290,7 @@ kAssesmentMSI <- function(path,
       
     }
     
-    #### generating the second output (class comparison input & distribution test)
+    #### generating the second output (Rowmeans input & distribution test)
     
     out_mat2 <- as.matrix(rowMeans(runner_WO_zeros))
     
@@ -334,7 +363,7 @@ kAssesmentMSI <- function(path,
     
     return(list_out)
     
-  } else if(is.null(returnObject) | returnObject == "ClassComparisonInput") {
+  } else if(is.null(returnObject) | returnObject == "RowMeansDataset") {
     
     return(list_out2)
     
